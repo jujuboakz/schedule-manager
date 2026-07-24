35fb3de4e0af62e959e8f6a9d551fe911ac26426
zhuyanyu <13826207822@163.com>
2026-07-24 14:02:27 +0800

    添加MainWindow类声明，实现构造函数和基础框架

---
+++ include/MainWindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QList>
+#include <QThread>
+#include "Task.h"
+
+class Reminder;  // 前置声明，等 thread 分支合并后再 include
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(const QString &username, QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    void onAddTask();
+    void onDeleteTask();
+    void onEditTask();
+    void onRefreshList();
+    void onDateSelected(const QDate &date);
+    void onToggleComplete();
+
+private:
+    void loadTasks();
+    void saveTasks();
+    void refreshTable(const QList<Task> &tasks);
+
+    Ui::MainWindow *ui;
+    QString m_username;
+    QList<Task> m_tasks;
+    int m_nextId;
+
+    QThread *m_remindThread;
+    Reminder *m_reminder;
+};
+
+#endif
